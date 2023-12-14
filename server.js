f1fdb8381144a894fe9ecd41c74098c6fab47284
PedroCosta2023 <lucaspedrolucio@gmail.com>
2023-12-14 12:28:45 -0300

    Aula 16

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose"); // Adicione essa linha
 
 const app = express();
 
 const dbConfig = require("./db");
 const roomsRoute = require("./routes/roomsRouter");
+const usersRoute = require("./routes/userRoute");
+
+// Conectar ao banco de dados
+mongoose.connect(dbConfig, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to the database"))
+  .catch((error) => console.error("Database connection error:", error));
 
 app.use(express.json());
 
 app.use("/api/rooms", roomsRoute);
+app.use("/api/users", usersRoute);
 
 const port = process.env.PORT || 5000;
 
